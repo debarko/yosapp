488191adf17e33a7949efe08ae4a136913866eb8
Tuhin Sengupta <tuhin3737@gmail.com>
2013-12-21 03:36:51 +0530

    ADD: chatroom primary layout

---
+++ chatroom.php
@@ -0,0 +1,49 @@
+<?php
+
+?>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/reset.css">
+		<link rel="stylesheet" type="text/css" media="all" href="css/chatroom.css">
+		<script language="javascript" type="text/javascript" src="js/library/jquery.min.js"></script>
+		<script language="javascript" type="text/javascript" src="js/chatroom.js"></script>
+	</head>
+	<body>
+	<div id="master">
+		<!-- header starts -->
+		<div id="header">
+			<div id="banner" >WHATSAPP ON WEB</div>
+			<div id="icons">
+				<div class="icon" id="logout"></div>
+				<div class="icon" id="settings"></div>
+				<div class="icon" id="connectivity"></div>
+			</div>
+		</div>
+		<!-- header ends -->
+		<!-- body starts here -->
+		<div id="bodybg">
+			<div id="leftpannel">
+				<div id="profileinfo"></div>
+				<div id="concactsearch"></div>
+				<div id="contactscontainer">
+					<ul>
+						<li class="contact"></li>
+						<li class="contact"></li>
+						<li class="contact"></li>
+						<li class="contact"></li>
+					</ul>
+				</div>
+				<div id="contactoptions"></div>
+			</div>
+		</div>
+
+
+
+		<div id="footer">
+
+		</div>
+	</div>
+		<div id="copyright"><p>Copyright &copy; 2013. TD projects. All rights reserved.</p></div>
+	</body>
+</html>
