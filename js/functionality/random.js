9ad680c0009547710c52aeb2d5d2d4e2b0e3678b
Tuhin Sengupta <tuhin3737@gmail.com>
2013-12-20 21:58:41 +0530

    Fix: FadeIn new content on Login

--- js/functionality/random.js
+++ js/functionality/random.js
@@ -1,8 +1,13 @@
-function maximize(){
-	$('#header').animate({height:"0px"},200,"linear",function(){
+function maximize(html_val){
+	$('#header').animate({height:"0px"},500,"linear",function(){
 		$('#header').css("display","none");
 	});
-	$('#footer').animate({height:"0px"},200,"linear",function(){
+	$('#footer').animate({height:"0px"},500,"linear",function(){
 		$('#footer').css("display","none");
+	});
+	$('#bodybg').animate({height:"100%"},500);
+	$('#bodybg').fadeOut(500, function(){
+		$('#bodybg').html(html_val);
+		$('#bodybg').fadeIn(400);
 	});
 }
