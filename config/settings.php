57cc51c7c9fdb968db10e524b186e0917a664a04
Tuhin Sengupta <tuhin3737@gmail.com>
2013-12-29 00:26:32 +0530

    FIX: left panel %age width and scrollbar customization

--- config/settings.php
+++ config/settings.php
@@ -3,8 +3,8 @@
 	error_reporting(E_ALL);
 	ini_set('display_errors', '1');
 	//All file paths should have the trailing /
-	define('CODES_LOCATION','/home/debarko/');
-	define('ROOT_PATH','/home/debarko/htdocs/whatsapponweb/');
+	define('CODES_LOCATION','/var/www/webwhatsaap/');
+	define('ROOT_PATH','/var/www/webwhatsaap/');
 	define('DEBUG_BUILD', true);
 	define('LOGIN_ATTEMPTS', 5);	
 ?>
