a8d8b9748977d17f46cbb8847057fa36813a15ce
Tuhin Sengupta <tuhin3737@gmail.com>
2013-12-31 15:08:16 +0530

    UPDATE: chat bubble and msg type bar(still working, just saving a draft)

--- template/chatscreen.php
+++ template/chatscreen.php
@@ -29,27 +29,27 @@
 </div>
 <div id="msgcontainer">
 	<br>
-	<div><div class="sentmsg"><div class="textbox">hi</div></div></div>
-	<div class="clearfix"><div class="recmsg"><div class="textbox">how are you?</div></div></div>
-	<div><div class="sentmsg"><div class="textbox">i'm fine</div></div></div>
-	<div class="clearfix"><div class="recmsg"><div class="textbox">i'm liking this whatsapp.</div></div></div>
-	<div ><div class="sentmsg"><div class="textbox">yo yo nigga</div></div></div>
-	<div class="clearfix"><div class="recmsg"><div class="textbox">whats cracking homie</div></div></div>
-	<div><div class="sentmsg"><div class="textbox">not much dogg</div></div></div>
-	<div class="clearfix"><div class="recmsg"><div class="textbox">cool</div></div></div>
-	<div><div class="sentmsg"><div class="textbox">send a long text just for testing</div></div></div>
-	<div class="clearfix"><div class="recmsg"><div class="textbox">Select methods to announce your stream. You can use SAP (Service Announce Protocol) or SLP (Service Location Protocol). You must also specify a channel name. The Mac OS X interface also allows you to export the description (SDP) file of a RTP session using the internal HTTP or RTSP server of VLC, or as a file. This can be done using the according checkboxes. The SDP URL text box allows to give the url or destination where the SDP file will be available.</div></div></div>
+	<div class="clearfix"><div class="sentmsg"><div class="textbox">hi<br>&nbsp;<span class="timestamp"></span><span class="deliverystatus"></span></div></div></div>
+	<div class="recmsg"><div class="textbox">how are you?</div></div>
+	<div class="clearfix"><div class="sentmsg"><div class="textbox">i'm fine</div></div></div>
+	<div><div class="recmsg"><div class="textbox">i'm liking this whatsapp.</div></div></div>
+	<div class="clearfix"><div class="sentmsg"><div class="textbox">yo yo nigga</div></div></div>
+	<div><div class="recmsg"><div class="textbox">whats cracking homie</div></div></div>
+	<div class="clearfix"><div class="sentmsg"><div class="textbox">not much dogg</div></div></div>
+	<div><div class="recmsg"><div class="textbox">cool</div></div></div>
+	<div class="clearfix"><div class="sentmsg"><div class="textbox">send a long text just for testing</div></div></div>
+	<div><div class="recmsg"><div class="textbox">Select methods to announce your stream. You can use SAP (Service Announce Protocol) or SLP (Service Location Protocol). You must also specify a channel name. The Mac OS X interface also allows you to export the description (SDP) file of a RTP session using the internal HTTP or RTSP server of VLC, or as a file. This can be done using the according checkboxes. The SDP URL text box allows to give the url or destination where the SDP file will be available.<span></span></div></div></div>
 	
-	<div><div class="sentmsg"><div class="textbox">hi</div></div></div>
-	<div class="clearfix"><div class="recmsg"><div class="textbox">how are you?</div></div></div>
-	<div><div class="sentmsg"><div class="textbox">i'm fine</div></div></div>
-	<div class="clearfix"><div class="recmsg"><div class="textbox">i'm liking this whatsapp.</div></div></div>
-	<div ><div class="sentmsg"><div class="textbox">yo yo nigga</div></div></div>
-	<div class="clearfix"><div class="recmsg"><div class="textbox">whats cracking homie</div></div></div>
-	<div><div class="sentmsg"><div class="textbox">not much dogg</div></div></div>
-	<div class="clearfix"><div class="recmsg"><div class="textbox">cool</div></div></div>
-	<div><div class="sentmsg"><div class="textbox">Select methods to announce your stream. You can use SAP (Service Announce Protocol) or SLP (Service Location Protocol). You must also specify a channel name. The Mac OS X interface also allows you to export the description (SDP) file of a RTP session using the internal HTTP or RTSP server of VLC, or as a file. This can be done using the according checkboxes. The SDP URL text box allows to give the url or destination where the SDP file will be available.</div></div></div>
-	<div class="clearfix"><div class="recmsg"><div class="textbox">Select methods to announce your stream. You can use SAP (Service Announce Protocol) or SLP (Service Location Protocol). You must also specify a channel name. The Mac OS X interface also allows you to export the description (SDP) file of a RTP session using the internal HTTP or RTSP server of VLC, or as a file. This can be done using the according checkboxes. The SDP URL text box allows to give the url or destination where the SDP file will be available.</div></div></div>
+	<div class="clearfix"><div class="sentmsg"><div class="textbox">hi</div></div></div>
+	<div><div class="recmsg"><div class="textbox">how are you?</div></div></div>
+	<div class="clearfix"><div class="sentmsg"><div class="textbox">i'm fine</div></div></div>
+	<div><div class="recmsg"><div class="textbox">i'm liking this whatsapp.</div></div></div>
+	<div class="clearfix"><div class="sentmsg"><div class="textbox">yo yo nigga</div></div></div>
+	<div><div class="recmsg"><div class="textbox">whats cracking homie</div></div></div>
+	<div class="clearfix"><div class="sentmsg"><div class="textbox">not much dogg</div></div></div>
+	<div><div class="recmsg"><div class="textbox">cool</div></div></div>
+	<div class="clearfix"><div class="sentmsg"><div class="textbox">Select methods to announce your stream. You can use SAP (Service Announce Protocol) or SLP (Service Location Protocol). You must also specify a channel name. The Mac OS X interface also allows you to export the description (SDP) file of a RTP session using the internal HTTP or RTSP server of VLC, or as a file. This can be done using the according checkboxes. The SDP URL text box allows to give the url or destination where the SDP file will be available.</div></div></div>
+	<div><div class="recmsg"><div class="textbox">Select methods to announce your stream. You can use SAP (Service Announce Protocol) or SLP (Service Location Protocol). You must also specify a channel name. The Mac OS X interface also allows you to export the description (SDP) file of a RTP session using the internal HTTP or RTSP server of VLC, or as a file. This can be done using the according checkboxes. The SDP URL text box allows to give the url or destination where the SDP file will be available.</div></div></div>
 	<br>
 </div>
 <div id="createmsgbar" class="clearfix">
