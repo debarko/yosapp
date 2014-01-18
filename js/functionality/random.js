eaf158dd859a34aa9e21ec28ecafe07228f41501
Debarko De <debarko89@gmail.com>
2014-01-18 11:10:21 +0530

    Fix: Focus not coming properly on login

--- js/functionality/random.js
+++ js/functionality/random.js
@@ -9,10 +9,11 @@ function maximize(html_val, callback){
 	$('#bodybg').fadeOut(500, function(){
 		$('#bodybg').html(html_val);
 		$('#bodybg').css('background-color','white');
-		$('#bodybg').fadeIn(400);
-		if(callback) {
-			callback();
-		}
+		$('#bodybg').fadeIn(400, function(){
+			if(callback) {
+				callback();
+			}
+		});
 	});
 	
 }
@@ -20,7 +21,7 @@ function maximize(html_val, callback){
 //Logs in a user upon successful login
 function log_in_user() {
 	maximize(YW.CHATSCREEN(), function(){
-		$('#typemsg').select('focus');
+		$('#typemsg').focus();
 		$('#icons').html(YW.LOGGED_IN_H());
 		$('#icons').css("top","2px");
 		$('#icons').css("right","2px");
