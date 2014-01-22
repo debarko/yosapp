27815f24fbd3c781e87924eb735ebf1daa43a2de
Tuhin Sengupta <tuhin3737@gmail.com>
2014-01-22 12:38:36 +0530

    FIX: some graphic fix

--- template/footer.php
+++ template/footer.php
@@ -4,4 +4,4 @@
 			<div></div>
 		</div>
 	</div>
-	<div id="copyright"><p>Copyright &copy; 2013. TD projects. All rights reserved.</p></div>
+	<div id="copyright"><p>Copyright &copy; 2013. folksFreek. All rights reserved.</p></div>
