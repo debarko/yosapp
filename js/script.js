b0bc3af8bec124a0a9f0ad62b15e89d1d4370939
Tuhin Sengupta <tuhin3737@gmail.com>
2014-01-28 21:51:49 +0530

    UPDATE: Add contact UI

--- js/script.js
+++ js/script.js
@@ -1062,26 +1062,18 @@ function selectMenuItem(selectedMenuItem){
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 
-/*function addContact(){
-	//animation part
-	var arrow = $('#addContactAnimation').find('#arrow');
-	for (var i=0; i<5; i++){ 
-		arrow.animate({left:'200px',opacity:'0'},300,'swing',function(){
-			arrow.css('left','0px');
-			arrow.css('opacity','1');
-		});
-	}
-}
-*/
 function addContact(){
-	//animation part
+	
 	var arrow = $('#addContactAnimation').find('#arrow');
 	//show contact name under the user icon
 	var fName = $('#contactFirstName').val();
 	var lName = $('#contactLastName').val();
+	var cCode = $('#countryCode').val();
+	var phNumber = $('#phNumber').val();
+	var cName = $('#countryName').val();
 	$('#contactNameAnimatedMsgBox').html(fName+' '+lName).animate({opacity:'1'},700, "swing");
-
 	
+	// animate the lower portion of the dialouge
 	animateLoop(5);
 	function animateLoop(times){
 		//break recurtion condition
@@ -1116,4 +1108,35 @@ function addContact(){
 			});
 		}
 	}
+	//the previous animation takes about 5 sec to complete
+	setTimeout(function() {
+		addedSuccessfullyAnimation();
+	}, 5000);
+
+	//second part of the animation
+	function addedSuccessfullyAnimation(){
+		//get the current state into a variable
+		var currentState = $('#addContactForm').html();
+		var animationContent =	'<div id="usderInfoHeader">CONTACT DETAILS</div>'
+								+'<div id="userInfoDiv">'
+									+'<div class="userInfoDivMemberwrapper"><div class="userInfoDivMemberIcons" id="userInfoDivMemberIconsUser"></div><div class="userInfoDivMemberText" id="userInfoDivMemberTextUsername"></div></div>'
+									+'<div class="userInfoDivMemberwrapper"><div class="userInfoDivMemberIcons" id="userInfoDivMemberIconsPhone"></div><div class="userInfoDivMemberText" id="userInfoDivMemberTextNumber"></div></div>'
+									+'<div class="userInfoDivMemberwrapper"><div class="userInfoDivMemberIcons" id="userInfoDivMemberIconsCountry"></div><div class="userInfoDivMemberText" id="userInfoDivMemberTextCountry"></div></div>'
+								+'</div>'
+								+'<div id="phonebookContainter"></div>'
+								+'<div id="successfullTag">Added To Phonebook Successfully</div>';
+		$('#addContactForm').fadeOut(500, function(){
+			$('#addContactForm').html(animationContent);
+			$('#addContactForm').fadeIn(400,function(){
+				//now put the name, phone number, cc and country name.
+				$('#userInfoDivMemberTextUsername').html(fName+' '+lName);
+				$('#userInfoDivMemberTextNumber').html('('+cCode+')'+' '+phNumber);  // format (+91) 9474070457
+				$('#userInfoDivMemberTextCountry').html(cName);
+
+			});
+		});
+
+	}
+	
+
 }
