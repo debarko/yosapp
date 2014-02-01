f076d2289b57b3ba5521940189a99c64f99cd38c
Tuhin Sengupta <tuhin3737@gmail.com>
2014-02-02 00:57:35 +0530

    ADD: verification UI (Still working)

--- template/homescreen.php
+++ template/homescreen.php
@@ -66,6 +66,37 @@
 <!-- uservarification temporary  -->
 <div id="varifyWindow">
 	<div id="varifyWindowContainer">
-		<div id="varifyWindowContainerHeader">VARIFICATION</div>
+		<div id="varifyWindowContainerHeader">VERIFICATION</div>
+		<div id="progBarBg">
+			<span class="progStepDot" id="fstStop">
+				<div class="innerDisk"></div>
+				<div class="stpDlgBox">
+					<div id="verHeaderStp1" class="verHeader">Choose Verifivation Method</div>
+					<div id="verStp1Left">
+						<div id="verStp1LeftTop"><p class="verText">Choose How verification code will be sent to you</p></div>
+						<div id="verStp1LeftBottom"><p class="verText">Code will be sent via SMS</p></div>
+					</div>
+					<div id="verStp1Right">
+						<div id="verSMSWrapper" class="verMethodSelectionWrapper"><div class="selectVerMethodCheckBox" id="selectSMS"></div><div class="selectIcons" id="selectIconsSMS"></div></div>
+						<div id="verVoiceWrapper" class="verMethodSelectionWrapper"><div class="selectVerMethodCheckBox" id="selectVOICE"></div><div class="selectIcons" id="selectIconsVOICE"></div></div>
+					</div>
+					<div class="nextStpArrow"></div>
+				</div>
+			</span>
+			<span class="progStepDot" id="sndStop">
+				<div class="innerDisk"></div>
+				<div class="stpDlgBox">
+					<div id="verHeaderStp2" class="verHeader">Enter Verification Code</div>
+					<div class="nextStpArrow"></div>
+				</div>
+			</span>
+			<span class="progStepDot" id="trdStop">
+				<div class="innerDisk"></div>
+				<div class="stpDlgBox">
+					<div id="verHeaderStp3" class="verHeader">Congratulation</div>
+					<div class="nextStpArrow"></div>
+				</div>
+			</span>
+		</div>
 	</div>
 </div>
