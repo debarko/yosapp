28b6dc18684dfa2c8de7ca91529d18d282b55d81
Tuhin Sengupta <tuhin3737@gmail.com>
2014-02-21 02:19:19 +0530

    ADD: question mark right next to whatsapp on web

--- template/footer.php
+++ template/footer.php
@@ -3,7 +3,7 @@
 		<div id="footer">
 			<div></div>
 			<div id="feedback" onmouseover='showPointerOnHover(this);' onclick='showModal("feedback");'><p>Feedback</p></div>
-			<div id="whatsapponwebText">Whatsapp On Web</div>
+			<div id="whatsapponwebText">WHATSAPP ON WEB<span>?</span></div>
 		</div>
 	</div>
 	<div id="copyright"><p>Copyright &copy; 2013. folksFreak. All rights reserved.</p></div>
