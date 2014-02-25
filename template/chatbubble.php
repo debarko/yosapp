5cb132fdf33d2f2f3dca8174f7822592e14d3f42
Debarko De <debarko89@gmail.com>
2014-02-25 14:29:22 +0530

    Fix: onclick triggering sharing in chatbubble

--- template/chatbubble.php
+++ template/chatbubble.php
@@ -1,7 +1,7 @@
 <div id="wrapper_bubble" class="clearfix">
 	<div id="mother_div" class="clearfix">
 		<div id="sent_recv" class="sent_recv">
-			<div class="textbox"  onclick="shareOption();">																	
+			<div class="textbox">																	
 			</div>
 			<span id="deliverystatus"></span>
 			<span id="timestamp" class="timestamp"></span>
