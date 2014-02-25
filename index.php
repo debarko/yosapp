bcc8b33315d2fa87d346b1405199e2c64db086ea
Tuhin Sengupta <tuhin3737@gmail.com>
2014-02-25 13:49:52 +0530

    FIX: uncomit

--- index.php
+++ index.php
@@ -20,20 +20,20 @@
 		<link rel="stylesheet" type="text/css" media="all" href="css/modal.css">
 		<link rel="stylesheet" type="text/css" media="all" href="css/minEmoji.css">
 		<!-- Third Party APIs -->
-		<!-- <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'> -->
-		<!-- <link href='http://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'> -->
-		<!-- <link href='http://fonts.googleapis.com/css?family=Sigmar+One' rel='stylesheet' type='text/css'> -->
+		<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Sigmar+One' rel='stylesheet' type='text/css'>
 		<!-- GoogleAnalyticsObject -->
 		<script>
-			// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			// ga('create', 'UA-48289387-1', 'yosapp.com');
-			// ga('send', 'pageview');
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-48289387-1', 'yosapp.com');
+			ga('send', 'pageview');
 		</script>
 
-		<!--<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/md5.js"></script> -->
+		<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/md5.js"></script>
 		<script language="javascript" type="text/javascript" src="js/library/jquery.min.js"></script>
 		<script language="javascript" type="text/javascript" src="js/library/minEmoji.js"></script>
 		<script language="javascript" type="text/javascript" src="js/library/notify/notify.js"></script>
