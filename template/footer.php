c7e9cdcbd246100bdebd780c637b720b71bf756d
Debarko De <debarko89@gmail.com>
2014-06-26 06:04:12 +0000

    Add: Firefox OS Icons & Manifest

--- template/footer.php
+++ template/footer.php
@@ -4,7 +4,7 @@
 			<div></div>
 			<div id="feedback" onmouseover='showPointerOnHover(this);' onclick='showModal("feedback");'><p>Feedback</p></div>
 			<div id="whatsapponwebText">WHATSAPP ON WEB<span onclick='showModal("vision");'>?</span></div>
-			<div id="copyright"><p>Copyright &copy; 2013. folksFreak. All rights reserved.</p></div>
+			<div id="copyright"><p>No Copyright. No Rights Reserved. Open Source Project. <a target="_blank" href="http://www.folksfreak.com">Folks Freak</a></p></div>
 		</div>
 	</div>
 	
