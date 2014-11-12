ce17b008ae010f75530fdefb57ebaffee0d5268a
Debarko De <debarko89@gmail.com>
2014-11-12 13:11:03 +0530

    Add: PDF and HTML links to footer

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,7 @@
 		<div id="footsep"></div>
 
 		<div id="footer">
-			<div style="text-align:center">We are going to shutdown Yosapp.com  at 10pm IST on 12th November 2014 after a legal intimidation from Whatsapp Inc. via Amazon.</div>
+			<div style="text-align:center">We are going to shutdown Yosapp.com  at 10pm IST on 12th November 2014 after a legal intimidation from Whatsapp Inc. via Amazon. [<a href="template/legalDoc.pdf" target="_blank">pdf</a>] [<a href="template/yosapplegal.html" target="_blank">html</a>]</div>
 			<div id="feedback" onmouseover='showPointerOnHover(this);' onclick='showModal("feedback");'><p>Feedback</p></div>
 			<div id="whatsapponwebText">WHATSAPP ON WEB<span onclick='showModal("vision");'>?</span></div>
 			<div id="copyright"><p>No Copyright. No Rights Reserved. Open Source Project. <a target="_blank" href="http://www.folksfreak.com">Folks Freak</a></p></div>
