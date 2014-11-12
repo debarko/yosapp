f33b5e2c31fbae6ec66a8c5c9795045a99ab60b3
Debarko De <debarko89@gmail.com>
2014-11-12 11:58:14 +0530

    Add: Closing message

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,7 @@
 		<div id="footsep"></div>
 
 		<div id="footer">
-			<div></div>
+			<div>We are going to shutdown Yosapp.com  at 10pm IST on 12th November 2014 after a legal intimidation from Whatsapp Inc. via Amazon.</div>
 			<div id="feedback" onmouseover='showPointerOnHover(this);' onclick='showModal("feedback");'><p>Feedback</p></div>
 			<div id="whatsapponwebText">WHATSAPP ON WEB<span onclick='showModal("vision");'>?</span></div>
 			<div id="copyright"><p>No Copyright. No Rights Reserved. Open Source Project. <a target="_blank" href="http://www.folksfreak.com">Folks Freak</a></p></div>
